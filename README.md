09be153b26378bf3631f668316794f503f27da15
vanvand <vanessa.domas@gmail.com>
2022-10-25 22:57:05 +0200

    Express Router, API Routes, MongoDB, Mongoose

--- README.md
+++ README.md
@@ -16,3 +16,24 @@ $ npm run dev (in backend/package.json add "dev": "nodemon server.js")
 $ npm i dotenv
 add .env file and variable for PORT
 add .gitignore file
+
+
+## 2- Express Router & API Routes
+
+### API Endpoints
+
+GET     /workouts       >> get all workouts docs
+POST    /workouts       >> creates a new workout doc
+GET     /workouts/:id   >> get a single workout doc
+DELETE  /workouts/:id   >> delete a single workout
+PATCH   /workouts/:id   >> update a single workout
+
+
+create routes/workouts.js
+and require created routes in server.js
+
+## 3- MongoDB & Mongoose
+
+create MongoDb project and database, get connection url for application and put in .env file
+$ npm i mongoose
+add connection config to server.js
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,33 @@
 require ("dotenv").config()
 
 const express = require("express")
+const mongoose = require("mongoose")
+const workoutRoutes = require("./routes/workouts")
 
 // express app
 const app = express()
 
-// global middleware to log path and method
-// middleware = any code that executes between us getting a request on the server and us sending a response
+// app.use() add a new middleware to the app > whenever request hits backend, express will execute functions passed to app.use() in order. 
+// express.json() parses incoming JSON requests and puts the parsed data in req.body.
+app.use(express.json())
+// global middleware to log path and HTTP method
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 // routes
-app.get("/", (req, res) => {
-    res.json({mssg: "Welcome to the app"})
-})
+app.use("/api/workouts", workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then( () => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`connected to db & listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
